4a61cfa2ec744ff48b52582eb296b31bbb600a0d
Paul Sanford <me@paulsanford.net>
2021-12-04 12:22:27 -0600

    Refactor with new stuff for day 1.2

--- day01.2/Day1.hs
+++ day01.2/Day1.hs
@@ -2,14 +2,18 @@ import GHC.IO.IOMode (IOMode (ReadMode))
 import System.IO (openFile, putStrLn)
 import Prelude hiding (putStrLn)
 
-countIncrease :: Int -> [Int] -> Int -> Int
-countIncrease prev xs count = if head xs > prev then count + 1 else count
+countIncrease :: Int -> Int -> Int -> Int
+countIncrease prev cur count
+  | cur > prev = count + 1
+  | otherwise = count
 
 countIncreases :: [Int] -> Int
-countIncreases xs = countIncreasesI (head xs) (tail xs) 0
+countIncreases [] = 0
+countIncreases (cur : xs) = countIncreasesI cur xs 0
 
 countIncreasesI :: Int -> [Int] -> Int -> Int
-countIncreasesI prev xs count = if null xs then count else countIncreasesI (head xs) (tail xs) (countIncrease prev xs count)
+countIncreasesI prev [] count = count
+countIncreasesI prev (cur : xs) count = countIncreasesI cur xs (countIncrease prev cur count)
 
 countFileIncreases :: String -> Int
 countFileIncreases xs = countIncreases (map (read :: String -> Int) (lines xs))
@@ -18,10 +22,12 @@ sumWindows :: [Int] -> [Int]
 sumWindows xs = reverse (sumWindows' xs [])
 
 sumWindows' :: [Int] -> [Int] -> [Int]
-sumWindows' xs agg = if length xs < 3 then agg else sumWindows' (tail xs) (sum (take 3 xs) : agg)
+sumWindows' xs agg
+  | length xs < 3 = agg
+  | otherwise = sumWindows' (tail xs) (sum (take 3 xs) : agg)
 
 main :: IO ()
 main = do
-  file_cont <- readFile "input.txt"
+  file_cont <- readFile "main.input"
   let count = countIncreases (sumWindows (map (read :: String -> Int) (lines file_cont)))
   print count
