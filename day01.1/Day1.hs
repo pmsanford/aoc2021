9afd20b81879f7d135cb208512b2bcbdab51f9db
Paul Sanford <me@paulsanford.net>
2021-12-04 12:18:47 -0600

    Refactor with new stuff for day 1

--- day01.1/Day1.hs
+++ day01.1/Day1.hs
@@ -2,20 +2,24 @@ import GHC.IO.IOMode (IOMode (ReadMode))
 import System.IO (openFile, putStrLn)
 import Prelude hiding (putStrLn)
 
-countIncrease :: Int -> [Int] -> Int -> Int
-countIncrease prev xs count = if head xs > prev then count + 1 else count
+countIncrease :: Int -> Int -> Int -> Int
+countIncrease prev cur count
+  | cur > prev = count + 1
+  | otherwise = count
 
 countIncreases :: [Int] -> Int
-countIncreases xs = countIncreasesI (head xs) (tail xs) 0
+countIncreases [] = 0
+countIncreases (cur : xs) = countIncreasesI cur xs 0
 
 countIncreasesI :: Int -> [Int] -> Int -> Int
-countIncreasesI prev xs count = if null xs then count else countIncreasesI (head xs) (tail xs) (countIncrease prev xs count)
+countIncreasesI prev [] count = count
+countIncreasesI prev (cur : xs) count = countIncreasesI cur xs (countIncrease prev cur count)
 
 countFileIncreases :: String -> Int
 countFileIncreases xs = countIncreases (map (read :: String -> Int) (lines xs))
 
 main :: IO ()
 main = do
-  file_cont <- readFile "input.txt"
+  file_cont <- readFile "main.input"
   let count = countFileIncreases file_cont
   print count
