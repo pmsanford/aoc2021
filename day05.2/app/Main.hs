860763963d15685dc9561c9c5354c5f76482538d
Paul Sanford <me@paulsanford.net>
2021-12-12 02:51:05 -0600

    Day 5.2

--- day05.2/app/Main.hs
+++ day05.2/app/Main.hs
@@ -1,30 +1,32 @@
 {-# LANGUAGE TupleSections #-}
-
 -- AOC 2021 Day 5
-
+import System.Environment
 import Data.List
+import Text.Regex.TDFA
 import qualified Data.Map.Strict as Map
 import Data.Maybe
-import System.Environment
-import Text.Regex.TDFA
 
 type Vent = ((Int, Int), (Int, Int))
 
 parseVent :: String -> Vent
-parseVent ln = mkTuples (map (read :: (String -> Int)) (getCaptures matchLine))
-  where
-    getCaptures (_, _, _, captures) = captures
-    matchLine = ln =~ "([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)" :: (String, String, String, [String])
-    mkTuples [x1, y1, x2, y2] = ((x1, y1), (x2, y2))
-    mkTuples _ = error "Wrong number of matches"
+parseVent ln = mkTuples (map (read::(String->Int)) (getCaptures matchLine))
+  where getCaptures (_, _, _, captures) = captures
+        matchLine = ln =~ "([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)" :: (String, String, String, [String])
+        mkTuples [x1, y1, x2, y2] = ((x1, y1), (x2, y2))
+        mkTuples _ = error "Wrong number of matches"
+
+genDiagonal :: Vent -> [(Int, Int)]
+genDiagonal ((x1, y1), (x2, y2)) = zip xrange yrange
+  where xrange = if x2 > x1 then [x1..x2] else reverse [x2..x1]
+        yrange = if y2 > y1 then [y1..y2] else reverse [y2..y1]
 
 genPoints :: Vent -> [(Int, Int)]
 genPoints ((x1, y1), (x2, y2))
-  | x1 == x2 && y1 < y2 = map (x1,) [y1 .. y2]
-  | x1 == x2 = map (x1,) [y2 .. y1]
-  | y1 == y2 && x1 < x2 = map (,y1) [x1 .. x2]
-  | y1 == y2 = map (,y1) [x2 .. x1]
-  | otherwise = []
+  | x1 == x2 && y1 < y2 = map (x1,) [y1..y2]
+  | x1 == x2 = map (x1,) [y2..y1]
+  | y1 == y2 && x1 < x2 = map (,y1) [x1..x2]
+  | y1 == y2 = map (,y1) [x2..x1]
+  | otherwise = genDiagonal ((x1, y1), (x2, y2))
 
 walkVents :: [Vent] -> Map.Map (Int, Int) Int
 walkVents = walkVents' Map.empty
@@ -34,15 +36,13 @@ addOne mv = Just (maybe 1 (+ 1) mv)
 
 walkVents' :: Map.Map (Int, Int) Int -> [Vent] -> Map.Map (Int, Int) Int
 walkVents' m (c : xs) = walkVents' (addPoints m (genPoints c)) xs
-  where
-    addPoints mm (cc : xss) = addPoints (Map.alter addOne cc mm) xss
-    addPoints mm [] = mm
+  where addPoints mm (cc : xss) = addPoints (Map.alter addOne cc mm) xss
+        addPoints mm [] = mm
 walkVents' m [] = m
 
 countMultiPoints :: Map.Map (Int, Int) Int -> Int
 countMultiPoints m = length $ Map.filterWithKey gt m
-  where
-    gt _ v = v > 1
+  where gt _ v = v > 1
 
 loadVents :: String -> IO [Vent]
 loadVents filePath = do
@@ -59,3 +59,4 @@ main :: IO ()
 main = do
   args <- getArgs
   loadRun (head args)
+
