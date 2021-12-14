78083e4ed8ef4b6c9db89ed647edbca2572972b6
Paul Sanford <me@paulsanford.net>
2021-12-13 22:44:26 -0600

    Day 8.1

---
+++ day08.1/app/Main.hs
@@ -0,0 +1,11 @@
+-- AOC 2021 Day 8
+import System.Environment
+import Data.List
+
+import Aoc
+
+main :: IO ()
+main = do
+  args <- getArgs 
+  loadRun (head args)
+
---
+++ day08.1/src/Aoc.hs
@@ -0,0 +1,20 @@
+module Aoc where
+import Data.List.Split
+
+countSegs :: [String] -> Int
+countSegs lns = length $ filter (\x -> length x < 5 || length x == 7) (concatMap (words . (!! 1) . splitOn "|") lns)
+
+loadLines :: String -> IO [String]
+loadLines filePath = do
+  fileCont <- readFile filePath
+  return $ lines fileCont
+
+loadRun :: String -> IO ()
+loadRun filePath = do
+  fileLines <- loadLines filePath
+  print $ Aoc.solve fileLines
+
+
+solve :: [String] -> Int
+solve = countSegs
+
